62ffc1bdd9830e3590c38cbc26dd97834c4af655
richardkchen <richardkchen@github.com>
2023-05-26 06:23:28 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿public interface ILetterService
+{
+  void CombineTwoLetters(string inputFile1, string inputFile2, string resultFile);
+}
+
+public class LetterService: ILetterService
+{
+  public void CombineTwoLetters(string inputFile1, string inputFile2, string resultFile)
+  {
+
+  }
+}
